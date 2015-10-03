779e5c5ec6ca3d5f5fd39f626533cf95c0dcfff8
owers19856 <owers19856@gmail.com>
2015-10-03 03:11:24 +0000

    Just corrected some errors with the last commit.

--- TBelec.py
+++ TBelec.py
@@ -13,8 +13,12 @@ import math
 import TBH
 import sys
 import time
-from myfunctions import fermi_0, fermi_non0
+import myfunctions
 from Verbosity import *
+import random
+
+# PyDQED module
+from pydqed import DQED
 
 class Electronic:
     """Initialise and build the density matrix."""
@@ -42,9 +46,16 @@ class Electronic:
         self.residue = np.zeros((self.Job.Def['num_rho'], self.Job.Hamilton.HSOsize, self.Job.Hamilton.HSOsize), dtype='complex')
 
         if self.Job.Def['el_kT'] == 0.0:
-            self.fermi = fermi_0
+            self.fermi = myfunctions.fermi_0
         else:
-            self.fermi = fermi_non0
+            self.fermi = myfunctions.fermi_non0
+
+        if self.Job.Def['optimisation_routine'] == 1:
+            self.optimisation_routine = self.optimisation_routine1
+        elif self.Job.Def['optimisation_routine'] == 2:
+            self.optimisation_routine = self.optimisation_routine2
+        else:
+            self.optimisation_routine = self.optimisation_routine3
 
 
     def occupy(self, s, kT, n_tol, max_loops):
@@ -95,20 +106,85 @@ class Electronic:
                 self.rho[TBH.map_atomic_to_index(atom1, orbital1, spin1, self.Job.NAtom, self.Job.NOrb),TBH.map_atomic_to_index(atom1, orbital2, spin2, self.Job.NAtom, self.Job.NOrb)]
            - self.rhotot[TBH.map_atomic_to_index(atom1, orbital1, spin1, self.Job.NAtom, self.Job.NOrb),TBH.map_atomic_to_index(atom1, orbital2, spin2, self.Job.NAtom, self.Job.NOrb)])
                 for atom1 in range(self.Job.NAtom) for orbital1 in range(self.Job.NOrb[atom1]) for spin1 in range(2)
-                for orbital2 in range(orbital1,self.Job.NOrb[atom1]) for spin2 in range(spin1,2)
+                for orbital2 in range(orbital1, self.Job.NOrb[atom1]) for spin2 in range(spin1, 2)
                 )/(self.Job.Electron.NElectrons**2)
 
-    def idempotency_error(self):
+    def idempotency_error(self, rho):
         """
         Determine how far from idempotency the density matrix is. If the
         density matrix is idempotent then
 
         rho*rho - rho = 0.
+
+        We normalise by the number of electrons.
         
         """
-        rho_err = np.linalg.norm((np.dot(self.rho, self.rho) - self.rho))
-        rhotot_err = np.linalg.norm((np.dot(self.rhotot, self.rhotot) - self.rhotot))
-        return rho_err, rhotot_err
+        rho_err = np.linalg.norm((np.dot(rho, rho) - rho))/self.NElectrons
+        return rho_err
+
+    def McWeeny(self):
+        """
+        Make the density matrix idempotent using the McWeeny transformation,
+        R.McWeeny, Rev. Mod. Phys. (1960):
+
+        rho_n+1 = 3*rho_n^3 - 2*rho_n^2
+
+        """
+        if self.Job.Def['Hamiltonian'] in ('scase','pcase','dcase','vectorS'):
+            rho_temp = self.rhotot
+        else:
+            rho_temp = self.rho
+
+        # Make sure that it isn't already idempotent
+        err_orig = self.idempotency_error(rho_temp)
+        if err_orig < self.Job.Def['McWeeny_tol']:
+            # if already idempotent then don't do anything, just exit function
+            return
+
+        flag, iterations, err, rho_temp = self.McWeeny_iterations(rho_temp)
+        # if the flag is false it means that idempotency was reduced below the tolerance
+        if flag == False:
+            # if the iterations did not converge but the idempotency error has
+            # gotten smaller then print a warning but treat as a success.
+            if err < err_orig:
+                print "Max iterations, ", iterations, " reached. Idempotency error = ", err
+                flag = True
+            else:
+                print "McWeeny transformation unsuccessful. Proceeding using input density matrix."
+                # Turn off using the McWeeny transformation as once it doesn't work it seems to not work again.
+                self.Job.Def["McWeeny"] = 0
+
+        # if this is going to be treated like a success then reassign rho_temp.
+        if flag == True:
+            if self.Job.Def['Hamiltonian'] in ('scase','pcase','dcase','vectorS'):
+                self.rhotot = rho_temp
+            else:
+                self.rho = rho_temp
+
+
+
+    def McWeeny_iterations(self, rho):
+        """
+        Iterations of the McWeeny scheme for the inputted rho.
+        Return a True/False flag that indicates convergence, the number of
+        iterations required to reach convergence, the error and the converged density
+        matrix.
+        """
+        converge_flag = False
+        for ii in range(self.Job.Def['McWeeny_max_loops']):
+            # McWeeny transformation
+            rho = 3*np.dot(rho, np.dot(rho, rho)) - 2*np.dot(rho, rho)
+            err = self.idempotency_error(rho)
+            verboseprint(self.Job.Def['extraverbose'], "McWeeny iteration: ", ii, "; Idempotency error = ", err)
+            if err < self.Job.Def['McWeeny_tol']:
+                converge_flag = True
+                return converge_flag, ii, err, rho
+            # Check to make sure that the error hasn't become a nan.
+            elif np.isnan(err):
+                return converge_flag, ii, err, rho
+
+        # if it gets to this statement then it probably hasn't converged.
+        return converge_flag, ii, err, rho
 
     def linear_mixing(self):
         """
@@ -161,10 +237,10 @@ class Electronic:
             # Guess for alpha is just 1.0 divided by the number of density matrices
             alpha = np.zeros((num_rho), dtype='double')
             alpha.fill(1.0/num_rho)
-        verboseprint(self.Job.Def['verbose'], "alpha: ", alpha)
+        verboseprint(self.Job.Def['extraverbose'], "alpha: ", alpha)
         # Create an optimised rhotot and an optimised rho and do linear mixing to make next input matrix
-        self.rhotot = sum(alpha[i]*self.inputrho[i] for i in range(len(alpha)))
-        self.rho = sum(alpha[i]*self.outputrho[i] for i in range(len(alpha)))
+        self.rhotot = sum(alpha[i]*self.inputrho[i] for i in range(num_rho))
+        self.rho = sum(alpha[i]*self.outputrho[i] for i in range(num_rho))
         self.linear_mixing()
 
 
@@ -280,7 +356,7 @@ class Electronic:
 
         return C_avg
 
-    def optimisation_routine(self, num_rho):
+    def optimisation_routine1(self, num_rho):
         """
         Optimisation routine where we try to solve for the norm squared of the
         optimal density matrix with the constraint that the sum of the
@@ -298,6 +374,7 @@ class Electronic:
         such that sum_i alpha_i = 1, which is equivalent to having solved for
         a different lambda.
         """
+        verboseprint(self.Job.Def['extraverbose'], "optimisation_routine")
         small = 1e-14
         # If there is only one density matrix the solution is simple.
         if num_rho == 1:
@@ -313,19 +390,232 @@ class Electronic:
                 # if np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])) != Mmat[i, j].conj():
                 #     print "Mmat[%i,%i] = %f. Mmat[%i,%i].conj() = %f." % (j, i, np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])), i, j, Mmat[i, j].conj())
                 Mmat[j, i] = Mmat[i, j].conj()
-        if np.linalg.det(Mmat) < small:
-            return alpha, 1
+        # if np.linalg.det(Mmat) < small:
+        #     return alpha, 1
 
         alpha = np.linalg.solve(Mmat, lamb)
         myscale = np.sum(alpha)
         if myscale == 0:
-            print "ERROR: alpha summed to 0 in optimisation_routine3. Cannot be scaled to 1."
+            print "ERROR: alpha summed to 0 in optimisation_routine. Cannot be scaled to 1."
             print alpha
             return alpha, 1
         else:
             alpha = alpha/myscale
         return alpha, 0
 
+    def optimisation_routine2(self, num_rho):
+        """
+        Optimisation routine where we try to solve for the norm squared of the
+        optimal density matrix with the constraint that the sum of the
+        coefficients is equal to one. To include the constraint we set up the
+        problem:
+
+        minimise: alpha_i M_ij alpha_j - lambda (sum_i alpha_i - 1)
+
+        where M_ij = Tr(R_i^dag R_j). We then differentiate with respect to
+        alpha_k and set to zero to minimise:
+
+        2 M alpha - lambda = 0
+
+        We solve this equation. We have to add a buffer row and column to
+        include lambda as well as the constraint that the sum of alpha is
+        equal to one. We absorb the 2 into lambda:
+
+        {M_11   M_12    ...    -1   {alpha_1     {0
+         M_21   M_22    ...    -1    alpha_2      0
+         .              .            .            .
+         .                .          .        =   .
+         .                  .        .            .
+         -1     -1      ...     0}   lambda}      -1}
+
+        """
+        small = 1e-10
+        verboseprint(self.Job.Def['extraverbose'], "optimisation_routine2")
+        # If there is only one density matrix the solution is simple.
+        if num_rho == 1:
+            return np.array([1.0], dtype='double'), 0
+
+        alpha = np.zeros(num_rho+1, dtype='double')
+        Mmat = np.matrix(np.zeros((num_rho+1, num_rho+1), dtype='complex'))
+        # make all the elements -1
+        Mmat.fill(-1.0)
+        # replace the bottom right hand corner by 0
+        Mmat[-1,-1] = 0.0
+        # calculate the rest of the Mmat.
+        for i in range(num_rho):
+            Mmat[i, i] = np.trace(np.matrix(self.residue[i])*np.matrix(self.residue[i]).H)
+            for j in range(i+1, num_rho):
+                Mmat[i, j] = np.trace(np.matrix(self.residue[i])*np.matrix(self.residue[j]).H)
+                # if np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])) != Mmat[i, j].conj():
+                #     print "Mmat[%i,%i] = %f. Mmat[%i,%i].conj() = %f." % (j, i, np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])), i, j, Mmat[i, j].conj())
+                Mmat[j, i] = Mmat[i, j].conj()
+        # if abs(np.linalg.det(Mmat)) < small:
+        #     return alpha, 1
+
+        RHS = np.zeros(num_rho+1, dtype = 'double')
+        RHS[-1] = -1.0
+        alpha = np.linalg.solve(Mmat, RHS)
+        myscale = abs(np.sum(alpha)-alpha[-1])
+        if abs(myscale-1.0) > small:
+            print "ERROR: optimisation_routine2 -- sum alpha = %f. alpha must sum to 1.0." % myscale
+            print alpha
+            return alpha, 1
+        # if successful then return result and no error code.
+        return alpha, 0
+
+
+    def optimisation_routine3(self, num_rho):
+        """
+        Solve the matrix vector equation approximately such that the alpha lie
+        between 0 and 1 and the constraint that sum_i alpha_i = 1:
+
+        {M_11   M_12    ...    M_1N  {alpha_1     {0
+         M_21   M_22    ...    M_2N   alpha_2      0
+         .              .                 .        .
+         .                .               .    =   .
+         .                  .             .        .
+         M_N1   M_N2           M_NN}  alpha_N}     0}
+
+        We use the library PyDQED to find a good solution with alpha_i bound
+        between 0 and 1. To ensure that alpha_i are bound between 0 and 1 we
+        replace alpha_i by sin^2(alpha_i). To ensure that sum_i alpha_i = 1 
+        we replace sin^2(alpha_i) by sin^2(alpha_i)/sum_a, where 
+        sum_a = sum_i sin^2(alpha_i).
+        """
+
+        verboseprint(self.Job.Def['extraverbose'], "optimisation_routine3")
+        # If there is only one density matrix the solution is simple.
+        if num_rho == 1:
+            return np.array([1.0], dtype='double'), 0
+
+        alpha = np.zeros(num_rho, dtype='double')
+        # initial guess for alpha:
+        alpha.fill(1.0/float(num_rho))
+        self.Mmat = np.zeros((num_rho, num_rho), dtype='complex')
+        # calculate Mmat.
+        for i in range(num_rho):
+            self.Mmat[i, i] = np.trace(np.matrix(self.residue[i])*np.matrix(self.residue[i]).H)
+            for j in range(i+1, num_rho):
+                self.Mmat[i, j] = np.trace(np.matrix(self.residue[i])*np.matrix(self.residue[j]).H)
+                # if np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])) != Mmat[i, j].conj():
+                #     print "Mmat[%i,%i] = %f. Mmat[%i,%i].conj() = %f." % (j, i, np.sum(np.matrix(self.residue[j]).H*np.matrix(self.residue[i])), i, j, Mmat[i, j].conj())
+                self.Mmat[j, i] = self.Mmat[i, j].conj()
+
+        # Initialise the PyDQED class       
+        opt = optimisation_rho_Duff()
+        opt.Mmat = self.Mmat
+        # bounds for the x values
+        # mybounds = [(0,1) for kk in range(num_rho)]
+        mybounds = None
+        # No bounds for lambda
+        # mybounds += [(None, None)]
+        # Strict bounds for the constraint
+        # mybounds += [(-1e-12, 1e-12)]
+        opt.initialize(Nvars=num_rho, Ncons=0, Neq=num_rho, bounds=mybounds, tolf=1e-16, told=1e-8, tolx=1e-8, maxIter=100, verbose=False)
+        alpha, igo = opt.solve(alpha)
+        if igo > 1:
+            verboseprint(self.Job.Def['extraverbose'], dqed_err_dict[igo])
+        # replace alpha[i] by sin^2(alpha[i])/sum_i sin^2(alpha[i])
+        sum_alpha = sum(np.sin(alpha[ii])*np.sin(alpha[ii]) for ii in range(num_rho))
+        alpha = np.array([np.sin(alpha[ii])*np.sin(alpha[ii])/sum_alpha for ii in range(num_rho)])
+        if abs(sum(alpha)-1.0) > 1e-8:
+            print "WARNING: sum_i alpha_i - 1.0 = " + str(sum_alpha-1.0) + ". It should be equal to 0.0. Proceeding using guess."
+            return alpha, 1
+
+        if self.Job.Def['random_seeds'] == 1:
+            # try the random seeds
+            trial_alpha, trial_cMc, trial_err = self.random_seeds_optimisation(num_rho, self.Job.Def['num_random_seeds'])
+
+            # Check to find out which solution is better, the guessed or the random seeds
+            cMc = alpha.conjugate().dot(self.Mmat.dot(alpha))
+            if cMc < trial_cMc:
+                return alpha, igo
+            else:
+                # print "Returning random seed answer. cMc = ", str(cMc), "; trial_cMc = ", str(trial_cMc)
+                return trial_alpha, trial_err
+
+
+        return alpha, igo
+
+
+    def random_seeds_optimisation(self, num_rho, num_trials):
+        cMc_vec = []
+        cMc_val = []
+        cMc_err = []
+        random.seed()
+        # Initialise the PyDQED class       
+        opt = optimisation_rho_Duff()
+        opt.Mmat = self.Mmat
+        mybounds = None
+        opt.initialize(Nvars=num_rho, Ncons=0, Neq=num_rho, bounds=mybounds, tolf=1e-16, told=1e-8, tolx=1e-8, maxIter=100, verbose=False)
+        # random starting seeds
+        for gg in range(num_trials):
+            alpha = np.array([random.random() for hh in range(num_rho)])
+            alpha, igo = opt.solve(alpha)
+            if igo > 1:
+                verboseprint(self.Job.Def['extraverbose'], dqed_err_dict[igo])
+            # replace alpha[i] by sin^2(alpha[i])/sum_i sin^2(alpha[i])
+            sum_alpha = sum(np.sin(alpha[ii])*np.sin(alpha[ii]) for ii in range(num_rho))
+            alpha = np.array([np.sin(alpha[ii])*np.sin(alpha[ii])/sum_alpha for ii in range(num_rho)])
+            cMc_vec.append(alpha)
+            cMc_val.append(alpha.conjugate().dot(self.Mmat.dot(alpha)))
+            cMc_err.append(igo)
+
+        # print "Trial values of cMc are: ", cMc_val
+        val, idx = min((val, idx) for (idx, val) in enumerate(cMc_val))
+        # print "chosen index = ", idx 
+        return cMc_vec[idx], cMc_val[idx], cMc_err[idx]
+
+
+class optimisation_rho_Duff(DQED):
+    """
+    A DQED class containing the functions to optimise.
+
+    It requires the small Mmat matrix to work.
+
+    {M_11   M_12    ...    M_1N
+     M_21   M_22    ...    M_2N
+     .              .          
+     .                .        
+     .                  .      
+     M_N1   M_N2           M_NN}
+
+    It implements the constraint that the sum_i x[i] = 1, and the bounds
+    0 < x[i] < 1 by replacing x[i] by sin^2(x[i])/sum_i sin^2(x[i]).
+    """
+    def evaluate(self, x):
+        Neq = self.Neq; Nvars = self.Nvars; Ncons = self.Ncons
+        f = np.zeros((Neq), np.float64)
+        J = np.zeros((Neq, Nvars), np.float64)
+        fcons = np.zeros((Ncons), np.float64)
+        Jcons = np.zeros((Ncons, Nvars), np.float64)
+
+
+        # Replace x[i] by sin^2(x[i])/sum_i sin^2(x[i])
+        y = []
+        sum_x = sum(np.sin(x[ii])*np.sin(x[ii]) for ii in range(Nvars-1))
+        for ii in range(Nvars):
+            y.append(np.sin(x[ii])*np.sin(x[ii])/sum_x)
+
+        for pp in range(Neq):
+            f[pp] = sum((self.Mmat[pp, ii])*y[ii] for ii in range(Nvars))
+            for kk in range(Nvars):
+                # find this equation by differentiating f[pp] w.r.t. x[kk]
+                J[pp, kk] = 2*np.sin(x[kk])*np.cos(x[kk])*(f[pp] - self.Mmat[pp, kk])/sum_x
+
+        return f, J, fcons, Jcons
+
+
+dqed_err_dict={}
+dqed_err_dict[2] = "The norm of the residual is zero; the solution vector is a root of the system."
+dqed_err_dict[3] = "The bounds on the trust region are being encountered on each step; the solution vector may or may not be a local minimum."
+dqed_err_dict[4] = "The solution vector is a local minimum."
+dqed_err_dict[5] = "A significant amount of noise or uncertainty has been observed in the residual; the solution may or may not be a local minimum."
+dqed_err_dict[6] = "The solution vector is only changing by small absolute amounts; the solution may or may not be a local minimum."
+dqed_err_dict[7] = "The solution vector is only changing by small relative amounts; the solution may or may not be a local minimum."
+dqed_err_dict[8] = "The maximum number of iterations has been reached; the solution is the best found, but may or may not be a local minimum."
+for ii in range(9,19):
+    dqed_err_dict[ii] = "An error occurred during the solve operation; the solution is not a local minimum."  
 
 
 
